ed18428ac5a7687f3b419b414bc64dabb83639f1
Dangol <sdangol@MGC12YV7K7LVCG.fbpld77.ford.com>
2020-06-23 17:47:08 -0400

    Initial commit

---
+++ Example/MyLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyLib
+//
+//  Created by SarmilaD on 06/23/2020.
+//  Copyright (c) 2020 SarmilaD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
